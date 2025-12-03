771a3234e293746f8436ddfe4ac40a08efd829dc
Meedoeed <skykk27@gmail.com>
2025-12-03 23:35:47 +0300

    simple-handler changes

--- simple-handler/main.go
+++ simple-handler/main.go
@@ -6,10 +6,20 @@ func greeting(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Добро пожаловать на главную страницу"))
 }
 
+func hello(w http.ResponseWriter, r *http.Request) {
+	name := r.URL.Query().Get("name")
+	if name == "" {
+		name += "незнакомец"
+	}
+	response := "Привет," + name + "!"
+	w.Write([]byte(response))
+}
+
 func main() {
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/", greeting)
+	mux.HandleFunc("/hello", hello)
 
 	http.ListenAndServe(":8080", mux)
 }
